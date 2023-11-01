5cff5fc62f0d180f8d24fb0d9db549c2ea2ac70a
SenneS <senne.de.neef@student.ehb.be>
2023-11-01 12:47:27 +0100

    - api 34 - splashscreen test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HetWeer
+https://developer.android.com/develop/ui/views/launch/splash-screen
+https://developer.android.com/develop/ui/views/launch/splash-screen/migrate
--- app/src/main/java/be/senne/meerweer/MainActivity.kt
+++ app/src/main/java/be/senne/meerweer/MainActivity.kt
@@ -1,6 +1,7 @@
 package be.senne.meerweer
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,10 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import be.senne.meerweer.ui.theme.HetWeerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val x = installSplashScreen();
+        Log.wtf("t", "abc = $x");
+
         super.onCreate(savedInstanceState)
         setContent {
             HetWeerTheme {
