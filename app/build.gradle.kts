a576fdf32103d41ba4bf432221e6b3fb5928d670
SenneS <senne.de.neef@student.ehb.be>
2023-11-05 13:38:16 +0100

    update
    - forgot to stage gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.4")
 
     implementation("com.google.dagger:hilt-android:2.48.1")
+    implementation("androidx.window:window:1.1.0")
     ksp("com.google.dagger:hilt-android-compiler:2.48.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
@@ -78,6 +79,8 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    implementation("androidx.compose.material3:material3-window-size-class")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
